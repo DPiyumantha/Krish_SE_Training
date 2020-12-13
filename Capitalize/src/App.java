4c48151b4b2c453b03a843681817db64e19d9a2b
dpiyumantha <dimalkapiyumantha@gmail.com>
2020-12-13 23:32:48 +0530

    implemented a hashmap

--- Capitalize/src/App.java
+++ Capitalize/src/App.java
@@ -8,18 +8,21 @@ import view.Interaction;
 public class App { 
 
 	public static void main(String[] args) {
-		ArrayList<String> stringLines, capitalisedStrings = new ArrayList<String>();
+		ArrayList<String> stringLines, capitalisedStrings, results = new ArrayList<String>();
 		FileDAO textDAO= new FileDAO();
 		try {
 			//Read data from file
 			stringLines = textDAO.getData();
 			
 			//Pass original data and get capitalised data
-			capitalisedStrings = Case.capitalizeAll(stringLines);
+			//capitalisedStrings = Case.capitalizeAll(stringLines);
+			stringLines.forEach((s)->{
+				results.add(Case.capita(s));
+			});
 			
 			//Print out the capitalised data, along with the original
 			//pass only capitalised data to print capitalised data only
-			Interaction.printResult(stringLines, capitalisedStrings);
+			Interaction.printResult(stringLines, results);
 			
 		} catch (IOException e) {
 			e.printStackTrace();
--- Capitalize/src/logical/Case.java
+++ Capitalize/src/logical/Case.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 public class Case {
 	
+	//this was my original solution but later i switched to hashmap
 	public static ArrayList<String> capitalizeAll(ArrayList<String> stringarr) {
 		ArrayList<String> stringLines = new ArrayList<String>();
 		stringarr.forEach((s)->{
@@ -14,6 +15,49 @@ public class Case {
 		return stringLines;
 		
 	}
-	
+
+	public static String capita(String s) {
+		String str ="";
+		char[] arr = new char[s.length()];
+		HashMap<Character, Character> alphabet = new HashMap<Character,Character>();
+		alphabet.put('a','A');
+		alphabet.put('b','B');
+		alphabet.put('c','C');
+		alphabet.put('d','D');
+		alphabet.put('e','E');
+		alphabet.put('f','F');
+		alphabet.put('g','G');
+		alphabet.put('h','H');
+		alphabet.put('i','I');
+		alphabet.put('j','J');
+		alphabet.put('k','K');
+		alphabet.put('l','L');
+		alphabet.put('m','M');
+		alphabet.put('n','N');
+		alphabet.put('o','O');
+		alphabet.put('p','P');
+		alphabet.put('q','Q');
+		alphabet.put('r','R');
+		alphabet.put('s','S');
+		alphabet.put('t','T');
+		alphabet.put('u','U');
+		alphabet.put('v','V');
+		alphabet.put('w','W');
+		alphabet.put('x','X');
+		alphabet.put('y','Y');
+		alphabet.put('z','Z');
+		arr =s.toCharArray();
+		for(int i=0; i<s.length();i++) {
+			if(alphabet.containsKey(arr[i])) {
+				arr[i] = alphabet.get(arr[i]);
+			}
+		}
+		
+		for(int i=0; i<s.length();i++) {
+			str+=arr[i];
+		}
+		return str;
+		
+	}
 
 }

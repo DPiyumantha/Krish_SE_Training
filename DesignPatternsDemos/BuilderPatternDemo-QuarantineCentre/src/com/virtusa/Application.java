df4c91c8d9472ae6bee2573466ae4839d3f5f282
dpiyumantha <dimalkapiyumantha@gmail.com>
2020-12-25 17:40:26 +0530

    cleaned

--- DesignPatternsDemos/BuilderPatternDemo-QuarantineCentre/src/com/virtusa/Application.java
+++ DesignPatternsDemos/BuilderPatternDemo-QuarantineCentre/src/com/virtusa/Application.java
@@ -9,6 +9,8 @@ public class Application {
 		
 		QuarantinedPerson person1 = shortTermQuarantine.name("Saman").nicNumber("5686164V").specialCareNeeded(true).build();
 		QuarantinedPerson person2 = mediumTermQuarantine.name("Anil").nicNumber("32475V").build();
+		
+		//Set country and passport number because j=he is a foreigner
 		QuarantinedPerson person3 = fullTermQuarantine.name("Jamie").country("Ireland").passportNumber("568616443543").specialCareNeeded(true).build();
 		
 		System.out.println(person1);
